cf073a434362e47ba7af56484bbb7503f5c4a0cc
Garrett Keyes <garrett.keyes@gmail.com>
2024-02-11 20:41:45 -0500

    Initial Commit

---
+++ GarrettCard/GarrettCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarrettCardApp.swift
+//  GarrettCard
+//
+//  Created by Garrett Keyes on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GarrettCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
